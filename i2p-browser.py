5803c5a5aa9b8a92a378fe9e1db8a26f13ca90aa
alyx <email@alxcore.org>
2023-09-29 17:54:59 -0500

    Add files via upload

--- i2p-browser.py
+++ i2p-browser.py
@@ -5,27 +5,27 @@ import argparse
 import os
 import time
 parser = argparse.ArgumentParser()
-parser.add_argument('--verbose', dest='verbose', action='store_true', help='Open terminal output for browser and i2pd router')
+parser.add_argument('--leech', dest='leech', action='store_true', help='Configure i2p to be a leech client rather than contribute to the network. (Not recommended but useful if integration time is slow.)')
 
+#   Test clean environment without nix installs
 
 args = parser.parse_args()
+
 class browser():
     def browserProc():
-        subprocess.call(['mullvad-browser', 'localhost:7070'])
+        subprocess.call(['mullvad-browser', '-P', 'i2pbrowser', '127.0.0.1:7070'])
         subprocess.call(['pkill', 'i2pd'])
     browser = threading.Thread(target=browserProc)
     
 class i2p():
     def i2pProc():
-        subprocess.Popen(['i2pd', '--ifname', 'tun0', '--bandwidth', 'X', '--socksproxy.enabled', '0', '--httpproxy.outproxy', 'http://outproxy.acetone.i2p:3128', '--httpproxy.inbound.length', '1', '--httpproxy.outbound.length', '1', '--http.showTotalTCSR', '1'])
+        if args.leech == True:
+            subprocess.Popen(['i2pd', '--notransit', '--socksproxy.enabled', '0', '--i2pcontrol.enabled', '1', '--i2cp.enabled', '1', '--httpproxy.outproxy', 'http://outproxy.acetone.i2p:3128', '--httpproxy.inbound.length', '1', '--httpproxy.outbound.length', '1', '--http.showTotalTCSR', '1'])
+        subprocess.Popen(['i2pd', '--bandwidth', 'O', '--socksproxy.enabled', '0', '--i2pcontrol.enabled', '1', '--i2cp.enabled', '1', '--httpproxy.outproxy', 'http://outproxy.acetone.i2p:3128', '--httpproxy.inbound.length', '1', '--httpproxy.outbound.length', '1', '--http.showTotalTCSR', '1'])
     i2p = threading.Thread(target=i2pProc)
 
 def startup():
-    #i2p.i2p.start()
-    #browser.browser.start()
     browser.browser.start()
-    #time.sleep(7)
     i2p.i2pProc()
-# close i2p with browser close... 
 
 startup()
